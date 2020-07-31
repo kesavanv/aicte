173baf067d87059d63792109a4207d07c36e16c5
Kesavan Varatharajan <kesavanv@gmail.com>
2020-07-31 18:16:53 +0530

    updated secondary pages

--- src/javascripts/common/helper.js
+++ src/javascripts/common/helper.js
@@ -1,20 +1,7 @@
-export const isFunction = (functionToCheck) => (functionToCheck && {}.toString.call(functionToCheck) === '[object Function]');
-
-export const hasProperty = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
-
-export const insert = (n, ins, arr) => [...arr.slice(0, n), ins, ...arr.slice(n)];
-
-export const simulateEmail = (name) => `${name.replace(/ +/g, '').toLowerCase()}@gmail.com`;
-
-export const getRandom = (list) => list[_.random(0, list.length - 1)];
+export const getRandom = (list) => list[Math.floor(Math.random() * list.length)];
 
 export const getRandomInt = (min, max) => {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 };
-
-export const simulatePhone = () => getRandomInt(9000000000, 9999999999);
-
-export const nTimesArray = (n, fn) => (Array.from(Array(n).keys()).map(() => fn()));
-
---
+++ src/partials/interns/profiles-list.js
@@ -0,0 +1,74 @@
+import config from './interns-list-config';
+import {
+  getRandomInt,
+} from '../../javascripts/common/helper';
+
+const { interns } = config;
+
+const intermediateBanner = (count) => {
+  const banner = `
+  <div class="intermediate-banner">
+    <h1>India's #1 <strong>Internship Portal</strong></h1>
+    <img class="banner-image" src="../../images/clipart-man.png" alt="" />
+  </div>
+  `;
+
+  return (count === 5) ? banner : '';
+};
+
+const loader = (internList, count) => {
+  const banner = `
+  <div class="loader">
+    <span><i class="fas fa-spinner"></i> Showing 10 / 1223 Profiles</span>
+  </div>
+  `;
+
+  return (count === (internList.length - 1)) ? banner : '';
+};
+
+const markup = `${interns.map((intern, count) => `
+${intermediateBanner(count)}
+<div class="profile-item ${intern.loading ? 'still-loading' : ''}">
+  <div class="row">
+    <div class="col-lg-6">
+      <div class="intern">
+        <img class="img-thumbnail" src="../../images/profile-photos/profile-photo${getRandomInt(1, 8)}.jpg" alt="">
+        <div class="intern-details">
+          <h6 class="intern-name">${intern.name}</h6>
+          <div class="description">
+            <p>
+              <span class="education">${intern.education}</span> | 
+              <span class="college">
+                <span class="name">${intern.collegeName}</span>,
+                <span class="location">${intern.location}</span>
+                <span class="score">${intern.score}</span> | 
+              </span>
+              <span class="interest">${intern.interest}</span>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="col-lg-6">
+      <div class="additional-attributes">
+        <div class="skills">
+          <h6>Skills Acquired</h6>
+          <p>${intern.skills}</p>
+        </div>
+
+        <div class="btn-wrap">
+          <button class="btn favourite ${intern.isFavourite ? 'active' : ''}"><i class="fas fa-star"></i></button>
+          <a href="intern-details.html" class="btn btn-primary view-btn ${intern.isViewed ? 'visited' : ''}">${intern.isViewed ? 'Viewed' : 'View'}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+${loader(interns, count)}
+`).join('')}`;
+
+export {
+  interns,
+  markup,
+};
